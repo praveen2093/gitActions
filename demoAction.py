4a711149fb84bd0728a579b25228f086895a90c1
Praveenkorepupk <praveenkorepu@gmail.com>
2026-09-05 06:01:51 +0000

    added python code

---
+++ demoAction.py
@@ -0,0 +1,7 @@
+def greet(name):
+    print(f"Hello, {name}!")
+    print("Welcome to the demo action.")
+    print("This is a simple greeting function.")
+    print("Feel free to modify it as needed.")
+    print(23+34)
+    print("Thank you for using the demo action.")
